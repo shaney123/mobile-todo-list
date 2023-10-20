76f7e8b08be96cd6d72166051f474564c3e620b4
Shaney <jenyshaneptagarda@gmail.com>
2023-10-20 09:22:46 +0800

    goods

--- App.js
+++ App.js
@@ -1,202 +1,275 @@
-import React, {useState} from 'react'; 
-import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import Task from './components/Task';
-import { useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Feather  from 'react-native-vector-icons/Feather'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { MaterialIcons } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
 
 
-
-
-
-
-
-
-
-export default function App() {
-  const [task, setTask] = useState('');      //we use state for the thngs that change often
-  const [taskItems, setTaskItems] = useState([]);   //this is the array to map the task in the screen
-  
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskText, setTaskText] = useState('');
+  const [editedTask, setEditedTask] = useState('');
+  const [editingTaskId, setEditingTaskId] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
-    const saveItems = async () => {
-      const saveDataInput = await AsyncStorage.getItem('taskItems');
-      if (saveDataInput !== null) {
-        setTaskItems(JSON.parse(saveDataInput));
-      }
-    };
-    saveItems();
-
+    getTasksFromUserDevice();
   }, []);
 
   useEffect(() => {
-    AsyncStorage.setItem('taskItems', JSON.stringify(taskItems));
-
-  }, [taskItems]);
-
-
-
-  const handleAddTask = () => {
-    Keyboard.dismiss();     //the keyboard goes up and down
-    setTaskItems([...taskItems, task])
-    setTask(null);      //to set the textinput empty
-  }
-
-  //const completeTask = (index) => { //to delete
-    //let itemsCopy = [...taskItems]; 
-
-
-
-  const handleDeleteTask = (index) => {
-    let itemsCopy = [...taskItems];
-    itemsCopy.splice(index, 1);
-    setTaskItems(itemsCopy);
+    saveTasksToUserDevice(tasks);
+  }, [tasks]);
   
-  }
-  const handleEditedTask = (task) => {
-    setTask(task);
-  }
+const addTask = () => {
+    const newTask = {
+      id: Math.random().toString(),
+      text: taskText,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+    setTaskText('');
   
-  
+};
+
+  const saveTasksToUserDevice = async (tasks) => {
+    try {
+      const stringifyTasks = JSON.stringify(tasks);
+      await AsyncStorage.setItem('tasks', stringifyTasks);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getTasksFromUserDevice = async () => {
+    try {
+      const tasks = await AsyncStorage.getItem('tasks');
+      if (tasks != null) {
+        setTasks(JSON.parse(tasks));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateTask = () => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === editingTaskId ? { ...task, text: editedTask } : task
+    );
+    setTasks(updatedTasks);
+    setEditingTaskId(null);
+    setIsEditing(false); 
+  };
+
+  const deleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
+  const toggleTaskCompletion = (taskId) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+ const renderTask = ({ item }) => (
+    <View style={styles.listItem}>
+      <TouchableOpacity
+        onPress={() => toggleTaskCompletion(item.id)}
+        style={[
+          styles.checkbox,
+          
+        ]}
+      >
+        {item.completed && (
+          <Icon name="check" size={24} color={'red'} />
+        )}
+      </TouchableOpacity>
+      {editingTaskId === item.id ? (
+        <TextInput
+          style={styles.inputText}
+          value={editedTask}
+          onChangeText={(text) => setEditedTask(text)}
+          onBlur={updateTask}
+        />
+      ) : (
+        <Text
+          style={[
+            styles.taskText,
+          ]}
+        >
+          {item.text}
+        </Text>
+      )}
+      {editingTaskId === item.id ? (
+        <TouchableOpacity
+          onPress={updateTask}
+          style={styles.updateButton}
+        >
+         <Icon name="update" size={25} color={'red'} />
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+          onPress={() => {
+            setEditingTaskId(item.id);
+            setEditedTask(item.text); 
+            setIsEditing(true);
+          }}
+          style={styles.editButton}
+        >
+         <Feather name="edit-3" size={24} color="black"/>
+        </TouchableOpacity>
+      )}
+      <TouchableOpacity
+        onPress={() => deleteTask(item.id)}
+        style={styles.deleteButton}
+      >
+         <MaterialIcons name="delete-forever" size={24} color="red" />
+      </TouchableOpacity>
+    </View>
+  );
 
   return (
     <View style={styles.container}>
-      {/*Today's Task */}
-      <View style={styles.tasksWrapper}>
-        <Text style={styles.sectionTitle}>Things To Do</Text> 
-
-          <View style={styles.items}>              
-            {/*This is where the tasks will go */}
-            {
-              taskItems.map((item, index) => { //maps the task in the screen
-                return (
-
-                  <TouchableOpacity key={index}>
-
-                    <View style={styles.icon}>
-                      
-                         <Task text={item} />
-                         
-                        <Feather name="edit-3" size={24} color="black"  onPress={() => handleEditedTask(item)}
-                        />
-                        
-                        <MaterialIcons name="delete-forever" size={24} color="red" onPress={() => handleDeleteTask(index)}/> 
-
-                           
-                    </View>
-
-                      
-
-                  </TouchableOpacity>
-
-                   
-
-                )
-           
-              })
-
-            }
-
-            {/*<Task text={'Task 1'} />
-            <Task text={'Task 2'} />
-          <Task text={'Task 3'} />
-
-            <TouchableOpacity key={index}  onPress={() => completeTask(index)}> 
-                    <Task text={item} />
-
-
-                  </TouchableOpacity>*/}
-
-
-
-          </View>
-
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Things To Do</Text>
       </View>
-
-      {/*Write a Task */}
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "android" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
-      >
-      <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)}/>
-      <TouchableOpacity onPress={() => handleAddTask()}>     
-        <View style={styles.addWrapper}>
-
-          <Text style={styles.addText}>Add</Text>
-
-
+      <FlatList
+        data={tasks}
+        renderItem={renderTask}
+        keyExtractor={(item) => item.id.toString()}
+      />
+      {!isEditing && (
+        <View style={styles.footer}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.inputText}
+              placeholder="Add task"
+              value={taskText}
+              onChangeText={(text) => setTaskText(text)}
+            />
+          </View>
+          <TouchableOpacity onPress={addTask} style={styles.iconContainer}>
+            <Icon name="add" size={24} color={'#fff'} />
+          </TouchableOpacity>
         </View>
-      </TouchableOpacity>
-
-      </KeyboardAvoidingView>
-
-
-
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#C0C0C0',
+    backgroundColor:'#C0C0C0',
   },
-  tasksWrapper: {
-    paddingTop: 50,
-    paddingHorizontal: 20,
-  },
-  sectionTitle: {
-    fontSize: 40,
-    fontWeight: 'bold',
-    fontStyle: 'italic',
-    textAlign: 'center',
-  
-  },
-  items: {
-    marginTop: 30,
-  },
-  writeTaskWrapper: {
-    position: 'absolute',
-    bottom: 15,
+ header: {
+  padding: 25,
+  justifyContent: 'center',
+  backgroundColor: '#C0C0C0',
+  margin: 15,
+  alignItems: 'center',
+},
+headerText: {
+  fontSize: 45,
+  fontWeight: 'bold',
+  color: '#000',
+},
+
+  footer: {
     width: '100%',
     flexDirection: 'row',
-    justifyContent: 'space-around',
     alignItems: 'center',
-
+    paddingHorizontal: 20,
+    backgroundColor: '#C0C0C0',
+    borderColor: '#fff'
+    ,
+    position: 'absolute',
+    bottom: 0,
   },
-  input: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-    width: 275,
-  
-
+  inputContainer: {
+    height: 50,
+    paddingHorizontal: 5,
+    backgroundColor: 'white',
+    flex: 1,
+    marginVertical: 35,
+    marginRight: 10,
+    borderRadius: 10,
+    justifyContent:'center',
+    alignItems: 'center',
+    flexDirection: 'row',
   },
-  addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
+  iconContainer: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
     justifyContent: 'center',
     alignItems: 'center',
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-
   },
-  addText: {
-    fontSize: 15,
-    
-  },
-
-  icon: {
+  listItem: {
+    padding: 25,
+    backgroundColor: '#fff',
     flexDirection: 'row',
-   
-    
+    borderRadius: 15,
+    marginVertical: 15,
+    alignItems: 'center',
   },
- 
-
+  checkbox: {
+    height: 24,
+    width: 24,
+    borderRadius: 9,
+    borderWidth: 1,
+    borderColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 8,
+  },
+  taskText: {
+    flex: 1,
+    fontWeight: 'thin',
+    fontSize: 15,
+    color: '#000',
+  },
+  inputText: {
+    flex: 1,
+    height: 40,
+    borderColor: 'gray',
+    borderRadius: 5,
+    paddingLeft: 5,
+    fontSize: 15,
+  },
+  editButton: {
+    paddingHorizontal: 6,
+    paddingVertical: 5,
+    borderRadius: 5,
+    marginRight: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  updateButton: {
+    paddingHorizontal: 6,
+    paddingVertical: 5,
+    borderRadius: 5,
+    marginRight: 3,
+    marginLeft: 8,
+  },
+  deleteButton: {
+   
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
+
+export default App;
+                                                                                  
